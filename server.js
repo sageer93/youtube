71dbe4c003bd892438e87e5c14bea651a7db0c64
sageer93 <128065898+sageer93@users.noreply.github.com>
2023-05-06 14:27:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ DB_Con()
 const {createServer} = require("http")
 const {Server} = require("socket.io")
 const server = createServer(app)
-const io = new Server(server,{cors:{origin:"*"}})
+const io = new Server(server,{cors:{origin:"https://youtube-royy.onrender.com/login"}})
 require("./auth/sockets")(io)
 server.listen(process.env.PORT||8080,()=>{
     console.log(`server running in port no ${process.env.PORT||8080}`)
